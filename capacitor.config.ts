08a5a47acb7e0a6efa10383705020e26294b6620
melrodriguez1133 <202000231@est.umss.est>
2026-08-12 15:41:51 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rick-morty-app',
+  webDir: 'www'
+};
+
+export default config;
